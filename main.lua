baf665550f30709457fd48b3b7837932873ff585
Paulo Carabalone <psoreto@outlook.com>
2016-01-15 14:43:10 -0200

    --
    Pode-se utilizar funções e métodos internos dentro das expressões.

--- main.lua
+++ main.lua
@@ -1,4 +1,46 @@
 --
+-- table.__basefs
+--
+-- Método de inicialização do processo
+--
+-- t    tabela
+-- e    expressão
+-- s    recursos de outro escopo
+-- return var, result, v, f, ov, rt
+--
+function table.__basefs(t, e, s)
+    local var, result, v = e:match('^(%w+)%s?'), {}, nil
+    local f = loadstring(e:gsub(var..'%s?[-=]>', 'return'))
+    local ov, rt = rawget(_G, var), {}
+    if s then
+        for k, v in pairs(s) do
+            rawset(rt, k, rawget(_G, k))
+            rawset(_G, k, v)
+        end
+    end
+    return var, result, v, f, ov, rt
+end
+
+--
+-- table.__basefe
+--
+-- Método de finalização do processo
+--
+-- var  nome da variável da expressão
+-- ov   valores antigos globais
+-- s    funções e recursos utilizados de outro escopo
+-- rt   tablea de valores antigos globais para funções
+--
+function table.__basefe(var, ov, s, rt)
+    if s then
+        for k, v in ipairs(s) do
+            rawset(_G, k, rawget(rt, k))
+        end
+    end
+    rawset(_G, var, ov)
+end
+
+--
 -- table.select
 --
 -- Itera sobre a table e filtra seus itens com a expressão definida.
@@ -7,16 +49,14 @@
 -- e    expressão
 -- return table
 --
-function table.where(t, e)
-    local var, result, v = e:match('^(%w+)%s?'), {}, nil
-    local f = loadstring(e:gsub(var..'%s?[-=]>', 'return'))
-    local ov = rawget(_G, var)
+function table.where(t, e, s)
+    local var, result, v, f, ov, rt = table.__basefs(t, e, s)
     for i = 1, #t do
         v = rawget(t, i)
         rawset(_G, var, v)
         if f() then rawset(result, #result + 1, v) end
     end
-    rawset(_G, var, ov)
+    table.__basefe(var, ov, s, rt)
     return result
 end
 
@@ -29,14 +69,12 @@ end
 -- e    expressão
 -- return table
 --
-function table.select(t, e)
-    local var, result, v = e:match('^(%w+)%s?'), {}, nil
-    local f = loadstring(e:gsub(var..'%s?[-=]>', 'return'))
-    local ov = rawget(_G, var)
+function table.select(t, e, s)
+    local var, result, v, f, ov, rt = table.__basefs(t, e, s)
     for i = 1, #t do
         rawset(_G, var, rawget(t, i))
         rawset(result, #result + 1, f())
     end
-    rawset(_G, var, ov)
+    table.__basefe(var, ov, s, rt)
     return result
 end
--- test.lua
+++ test.lua
@@ -1,7 +1,11 @@
 require 'main'
 
+function incr(a)
+    return a + 1
+end
+
 local x = setmetatable({1, 2, 3, 4, 5, 6, 7, 8, 9}, {__index = table})
 local t = x:where 'exp -> exp >= 5'
-local z = x:select 'x -> x + 0'
+local z = x:select('x -> incr(x)', {incr = incr})
 
-for _, i in ipairs(z) do print(i) end
+for _, i in ipairs(t) do print(i) end

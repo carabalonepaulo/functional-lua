d88cf18f4c1a00487edd07a01e94520e8c3c0dfb
Paulo Carabalone <psoreto@outlook.com>
2016-01-15 02:56:28 -0200

    substituição de métodos

--- main.lua
+++ main.lua
@@ -1,8 +1,9 @@
 function table.where(t, e)
-    local var, f, result = e:sub(1, 1), loadstring('return '..e), {}
-    for _, i in pairs(t) do
-        rawset(_G, var, i)
-        if f() then rawset(result, #result + 1, i) end
+    local var, f, result, v = e:sub(1, 1), loadstring('return '..e), {}, nil
+    for i = 1, #t do
+        v = rawget(t, i)
+        rawset(_G, var, v)
+        if f() then rawset(result, #result + 1, v) end
     end
     rawset(_G, var, nil)
     return result
@@ -10,8 +11,8 @@ end
 
 function table.select(t, e)
     local var, f, result = e:sub(1, 1), loadstring('return '..e), {}
-    for _, i in pairs(t) do
-        rawset(_G, var, i)
+    for i = 1, #t do
+        rawset(_G, var, rawget(t, i))
         rawset(result, #result + 1, f())
     end
     rawset(_G, var, nil)
@@ -22,4 +23,4 @@ local tabl = {1, 2, 3, 4, 5, 6, 7, 8, 9}
 local t = table.where(tabl, 'x >= 5')
 local x = table.select(tabl, 'x + 1')
 
-for _, i in pairs(x) do print(i) end
+for _, i in pairs(t) do print(i) end
